6c04a7f4c291fdeae9973928469802cc92020bff
Patrick <patrick@agora.studio>
2020-11-01 22:14:26 -0500

    Adding uart, callbacks

--- examples/esp32/main/main.cpp
+++ examples/esp32/main/main.cpp
@@ -7,9 +7,23 @@
 #include <freertos/task.h>
 #include <nvs_flash.h>
 #include <protocol_examples_common.h>
+#include "driver/uart.h"
+#include <stdio.h>
 
 #define LED GPIO_NUM_2
 #define PRINT_LINK_STATE false
+
+// Serial midi
+#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_TXD  (GPIO_NUM_4)
+#define ECHO_TEST_RXD  (GPIO_NUM_5)
+#define BUF_SIZE (1024)
+#define MIDI_TIMING_CLOCK 0xF8
+#define MIDI_START 0xFA
+#define MIDI_STOP 0xFC
+#define MIDI_SONG_POSITION_POINTER 0xF2
+
 
 unsigned int if_nametoindex(const char* ifName)
 {
@@ -71,25 +85,75 @@ void printTask(void* userParam)
   }
 }
 
+// callbacks
+void tempoChanged(double tempo) {
+  std::cout << std::defaultfloat << "| new tempo: " << tempo << std::endl;
+  /*
+  //MIDI CLOCK
+  uart_write_bytes(UART_NUM_1, (const char *) MIDI_TIMING_CLOCK, 1);
+  uart_write_bytes(UART_NUM_1, 0, 1);
+  */
+}
+
+bool lastState = false;
+void startStopChanged(bool isPlaying) {
+  // received as soon as sent
+  // need to wait for phase to be 0 (and deal with latency...)
+  std::cout << std::defaultfloat << "| state: " << isPlaying << std::endl;
+
+  if(isPlaying != lastState) {
+    if(isPlaying) {
+      uart_write_bytes(UART_NUM_1, (const char *) MIDI_START, 1);
+      uart_write_bytes(UART_NUM_1, 0, 1);
+    } else {
+      uart_write_bytes(UART_NUM_1, (const char *) MIDI_STOP, 1);
+      uart_write_bytes(UART_NUM_1, 0, 1);
+    }
+    lastState = isPlaying;
+  }
+}
+
 void tickTask(void* userParam)
 {
+
+  // serial
+  uart_config_t uart_config = {
+    .baud_rate = 31250, // midi speed
+    .data_bits = UART_DATA_8_BITS,
+    .parity    = UART_PARITY_DISABLE,
+    .stop_bits = UART_STOP_BITS_1,
+    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    .rx_flow_ctrl_thresh = 122,
+  };
+  uart_param_config(UART_NUM_1, &uart_config);
+  uart_set_pin(UART_NUM_1, ECHO_TEST_TXD, ECHO_TEST_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+  uart_driver_install(UART_NUM_1, BUF_SIZE * 2, 0, 0, NULL, 0);
+
+  // connect link
   ableton::Link link(120.0f);
   link.enable(true);
+  link.enableStartStopSync(true); // if not no callback for start/stop
 
+  // callbacks
+  link.setTempoCallback(tempoChanged);
+  link.setStartStopCallback(startStopChanged);
+
+  // debug
   if (PRINT_LINK_STATE)
   {
     xTaskCreate(printTask, "print", 8192, &link, 1, nullptr);
   }
-
   gpio_set_direction(LED, GPIO_MODE_OUTPUT);
 
+  // phase
   while (true)
-  {
+  { 
     xSemaphoreTake(userParam, portMAX_DELAY);
 
     const auto state = link.captureAudioSessionState();
     const auto phase = state.phaseAtTime(link.clock().micros(), 1.);
     gpio_set_level(LED, fmodf(phase, 1.) < 0.1);
+
     portYIELD();
   }
 }
@@ -105,4 +169,5 @@ extern "C" void app_main()
   timerGroup0Init(100, tickSemphr);
 
   xTaskCreate(tickTask, "tick", 8192, tickSemphr, configMAX_PRIORITIES - 1, nullptr);
+
 }
